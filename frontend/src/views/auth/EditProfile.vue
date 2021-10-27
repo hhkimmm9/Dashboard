3f18cf308ffed603656de82427d48d1a5972ef61
Harrison Kim <hsookim90@gmail.com>
2021-10-26 23:14:56 -0400

    Add EditProfile component.

---
+++ frontend/src/views/auth/EditProfile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h3>Profile Edit</h3>
+
+    <router-link to="/user/login">Go back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileEdit',
+}
+</script>
+
+<style></style>
