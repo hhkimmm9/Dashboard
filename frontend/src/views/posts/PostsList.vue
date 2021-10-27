8abf7cc31a30f636bd2a260aedf7ee384d2d6311
Harrison Kim <hsookim90@gmail.com>
2021-10-26 22:39:33 -0400

    Fix CSS for post-related components.

--- frontend/src/views/posts/PostsList.vue
+++ frontend/src/views/posts/PostsList.vue
@@ -1,41 +1,36 @@
 <template>
   <div id="container">
-    <h1>Market</h1>
+    <h1>Post List</h1>
 
     <!-- Only if logged in -->
     <button @click="clickHandler" v-if="signedIn">Post</button>
 
-    <div v-for="post in getPosts" :key="post.id">
-      <Post :postItem="post" />
+    <div id="post-card" v-for="post in getPosts" :key="post.id">
+      <PostCard :postItem="post" />
     </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
-import Post from './Post'
+import PostCard from './PostCard'
 
 export default {
-  name: 'Market',
-
+  name: 'PostList',
   data() {
     return {
       signedIn: false,
     }
   },
-
   components: {
-    Post,
+    PostCard,
   },
-
   methods: {
     ...mapActions(['fetchPosts']),
-
     clickHandler() {
       this.$router.replace('/post/upload')
     },
   },
-
   mounted() {
     this.fetchPosts()
 
@@ -43,12 +38,16 @@ export default {
       this.signedIn = true
     }
   },
-
   computed: mapGetters(['getPosts']),
 }
 </script>
 
 <style scoped>
 #container {
+  padding: 20px;
+}
+
+#post-card {
+  margin: 20px;
 }
 </style>
