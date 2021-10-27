0748070454b7638db72636baae3e5a3af8edd0dc
Harrison Kim <hsookim90@gmail.com>
2021-10-26 22:38:53 -0400

    Fix CSS for todo-related components.
    Change file names as well for improved understandability.

---
+++ frontend/src/views/todos/NewTodoForm.vue
@@ -0,0 +1,83 @@
+<template>
+  <form @submit="onSubmit">
+    <div class="input-group">
+      <label for="form-title">Title</label>
+      <input
+        v-model="form.title"
+        placeholder="Enter title..."
+        id="form-title"
+        type="text"
+        required
+      />
+    </div>
+    <div class="input-group">
+      <label for="form-desc">Description</label>
+      <input
+        v-model="form.description"
+        placeholder="Enter description..."
+        id="form-dsec"
+        type="text"
+        reqired
+      />
+    </div>
+    <div class="flag-section">
+      <input v-model="form.flag" id="form-flag" type="checkbox" value="flag" />
+      <label for="form-flag">Flag</label>
+    </div>
+    <input type="submit" value="Add todo" />
+  </form>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'NewTodoForm',
+
+  data() {
+    return {
+      form: {
+        title: '',
+        description: '',
+        flag: false,
+      },
+    }
+  },
+
+  methods: {
+    ...mapActions(['addTodo']),
+
+    //
+    onSubmit(event) {
+      event.preventDefault()
+      // alert(JSON.stringify(this.form))
+
+      this.addTodo({
+        title: this.form.title,
+        description: this.form.description,
+        flag: this.form.flag,
+      })
+
+      this.form.title = ''
+      this.form.description = ''
+      this.form.flag = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+.input-group {
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+}
+
+.flag-section > label {
+  padding: 5px;
+}
+
+form > input {
+  padding: 3px;
+}
+</style>
