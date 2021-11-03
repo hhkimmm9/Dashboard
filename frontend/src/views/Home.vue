4328243aa56320bc84ccd5d6b3ce3905aad6e1d6
Harrison Kim <hsookim90@gmail.com>
2021-11-02 20:37:16 -0400

    Set to show profile picture on home.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <h3>Home</h3>
+    <img
+      :src="`http://localhost:5000/${getUserInfo.profilePicture}`"
+      alt="user profile picture"
+    />
     <p>
       {{ getIsSignedIn ? `Hello Welcome back, ${getUserInfo.username}!` : '' }}
     </p>
