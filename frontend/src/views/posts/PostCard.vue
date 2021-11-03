ab65b665ba3287366f2f5240ffbd2b716fee911a
Harrison Kim <hsookim90@gmail.com>
2021-11-03 19:56:42 -0400

    Load image path of each post from prop instead of wrong place.
    wrong place means the getter that I used for each post detail.

--- frontend/src/views/posts/PostCard.vue
+++ frontend/src/views/posts/PostCard.vue
@@ -1,9 +1,6 @@
 <template>
   <div @click="readPostDetail" class="post-card">
-    <img
-      :src="`http://localhost:5000/${getPostDetail.image}`"
-      alt="product image"
-    />
+    <img :src="`http://localhost:5000/${postItem.image}`" alt="product image" />
 
     <div class="product-detail">
       <h2>{{ postItem.title }}</h2>
