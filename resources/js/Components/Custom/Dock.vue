8b3ce2b0a9ab427b338e5377bf0103b073e5fb01
Harrison Kim <hsookim90@gmail.com>
2023-02-09 16:21:05 -0500

    Replace img with icons

--- resources/js/Components/Custom/Dock.vue
+++ resources/js/Components/Custom/Dock.vue
@@ -5,19 +5,22 @@ var apps = [
     {
         id: 1,
         name: "Dashboard",
-        url: ""
+        icon: "dashboard",
     },
     {
         id: 2,
         name: "BlockSix",
+        icon: "grid_view",
     },
     {
         id: 3,
         name: "Calendar",
+        icon: "calendar_month",
     },
     {
         id: 4,
         name: "Tetris",
+        icon: "sports_esports",
     },
 ]
 </script>
@@ -27,10 +30,8 @@ var apps = [
         <div v-for="item in apps" :key="item.id"
             class="px-2 hover:shadow-2xl hover:bg-zinc-400 cursor-pointer py-1 space-y-1"
         >
-            <Link href="#" :class="[route().current('dashboard') ? '' : '']">
-                <img src="https://images.squarespace-cdn.com/content/v1/5ab26f4ef2e6b166c6b17a79/f1e4d05b-da9b-453b-a53a-7254e96c7ecd/logo2.png" alt=""
-                    class="w-10 mx-auto"
-                >
+            <Link href="#" :class="[route().current('dashboard') ? '' : '', 'flex flex-col']">
+                <span class="material-icons mx-auto text-3xl">{{ item.icon }}</span>
                 <div class="text-center"> {{ item.name }} </div>
             </Link >
         </div>
