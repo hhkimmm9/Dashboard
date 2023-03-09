b161d3afa1f24410f57684371986661a62329878
Harrison Kim <hsookim90@gmail.com>
2023-03-09 11:03:29 -0500

    Add modals for quick-edit and comments

---
+++ resources/js/Components/BlockSix/TaskQuickEditModal.vue
@@ -0,0 +1,46 @@
+<template>
+    <Modal :show="layoutStore.open_quick_edit_modal">
+        <div class="space-y-4 p-5">
+            <div class="flex flex-col space-y-2 border border-gray-200 rounded px-4 py-5 bg-gray-100">
+                <label for="keyword"> Keyword </label>
+                <input type="text" :value="generalStore.modal_target_container.keyword" class="border-none focus:ring-blue-500 focus:ring-2">
+            </div>
+            <div class="flex flex-col space-y-2 border border-gray-200 rounded px-4 py-5 bg-gray-100">
+                <label for="description"> Short Description </label>
+                <input type="text" :value="generalStore.modal_target_container.description" class="border-none focus:ring-blue-500 focus:ring-2">
+            </div>
+
+            <div class="flex justify-end space-x-3">
+                <button @click="onCloseModal" class="border border-gray-200 px-3 py-2 rounded font-bold text-gray-600 bg-gray-50 hover:bg-gray-100 capitalize">
+                    cancel
+                </button>
+                <button @click="updateTask" class="bg-blue-400 border border-blue-300 px-3 py-2 rounded text-white font-bold hover:bg-blue-500 capitalize">
+                    update
+                </button>
+            </div>
+        </div>
+        
+    </Modal>
+</template>
+
+<script setup>
+import Modal from '@/Components/Modal.vue'
+import { useGeneralStore, useLayoutStore, } from '@/Stores/index'
+
+const layoutStore = useLayoutStore();
+const generalStore = useGeneralStore();
+
+const updateTask = () => {
+    console.log('update task clicked!');
+};
+
+const onCloseModal = () => {
+    layoutStore.open_quick_edit_modal = false;
+    generalStore.modal_target_container = null;
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/Pages/BlockSix/Index.vue
+++ resources/js/Pages/BlockSix/Index.vue
@@ -1,11 +1,12 @@
 <template>
-    <Head title="BlockSix (index)" />
+    <div>
+        <Head title="BlockSix (index)" />
 
-    <AuthenticatedLayout class="relative">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-32 h-full py-10 flex flex-row">
+        <AuthenticatedLayout class="relative">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-32 h-full py-10 flex flex-row">
 
-            <div class="relative bg-white border border-gray-200 shadow w-2/3 mx-auto p-6 flex flex-col space-y-4 rounded-lg">
-                <div class="font-bold text-2xl"> {{ date }} </div>
+                <div class="relative bg-white border border-gray-200 shadow w-2/3 mx-auto p-6 flex flex-col space-y-4 rounded-lg">
+                    <div class="font-bold text-2xl"> {{ date }} </div>
                     <div>
                         <TaskContainer label="Morning" :first_task="tasks.data[0]" :second_task="tasks.data[1]" />
                         <hr class="my-4">
@@ -14,21 +15,24 @@
                         <TaskContainer label="Evening" :first_task="tasks.data[4]" :second_task="tasks.data[5]" />
                     </div>
 
-                <Link :href="tasks.next_page_url" :class="['absolute -left-10 top-48', tasks.next_page_url ? 'visible' : 'hidden']">
-                    <button class="">
-                        <span class="material-symbols-outlined">keyboard_double_arrow_left</span>
-                    </button>
-                </Link>
+                    <Link :href="tasks.next_page_url" :class="['absolute -left-10 top-48', tasks.next_page_url ? 'visible' : 'hidden']">
+                        <button class="">
+                            <span class="material-symbols-outlined">keyboard_double_arrow_left</span>
+                        </button>
+                    </Link>
 
-                <Link :href="tasks.prev_page_url" :class="['absolute -right-10 top-48', tasks.prev_page_url ? 'visible' : 'hidden']">
-                    <button class="">
-                        <span class="material-symbols-outlined">keyboard_double_arrow_right</span>
-                    </button>
-                </Link>
+                    <Link :href="tasks.prev_page_url" :class="['absolute -right-10 top-48', tasks.prev_page_url ? 'visible' : 'hidden']">
+                        <button class="">
+                            <span class="material-symbols-outlined">keyboard_double_arrow_right</span>
+                        </button>
+                    </Link>
+                </div>
             </div>
+        </AuthenticatedLayout>
 
-        </div>
-    </AuthenticatedLayout>
+        <TaskComment />
+        <TaskQuickEdit />
+    </div>
 </template>
 
 <script setup>
@@ -36,6 +40,8 @@ import { Head, Link } from '@inertiajs/vue3';
 import { ref, computed } from 'vue';
 import { useTippy } from 'vue-tippy';
 import TaskContainer from '@/Components/BlockSix/TaskContainer.vue';
+import TaskComment from '@/Components/BlockSix/TaskCommentModal.vue'
+import TaskQuickEdit from '@/Components/BlockSix/TaskQuickEditModal.vue'
 
 const props = defineProps(['tasks']);
 
