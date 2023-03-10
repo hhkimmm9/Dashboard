580141435fdc97899c91eb12dc11acb6aac4cc82
Harrison Kim <hsookim90@gmail.com>
2023-03-09 19:09:32 -0500

    Make a task desc clickable and it will redirect users to show page

--- resources/js/Components/BlockSix/TaskContainer.vue
+++ resources/js/Components/BlockSix/TaskContainer.vue
@@ -8,10 +8,10 @@
                     as="input" type="checkbox" :checked="first_task.is_completed"
                 />
                 <div class="w-full flex flex-row justify-between items-center">
-                    <div class="flex flex-row space-x-1">
+                    <Link :href="`blocksix/${first_task.id}`" method="GET" as="div" class="flex flex-row space-x-1 cursor-pointer">
                         <div class="bg-yellow-200 text-violet-500 px-1"> {{ first_task.keyword }} </div>
                         <div :class="first_task.is_completed ? 'line-through' : ''">{{ first_task.description }}</div>
-                    </div>
+                    </Link>
                     <div class="flex flex-row space-x-2 cursor-pointer">
                         <span @click="prepareOpenQuickEditModal(first_task)" class="material-symbols-outlined text-xl opacity-50 hover:opacity-80">edit</span>
                         <span @click="prepareOpenCommentModal(first_task)" class="material-symbols-outlined text-xl opacity-50 hover:opacity-80">add_comment</span>
@@ -24,10 +24,10 @@
                     as="input" type="checkbox" :checked="second_task.is_completed"
                 />
                 <div class="w-full flex flex-row justify-between items-center">
-                    <div class="flex flex-row space-x-1">
+                    <Link :href="`blocksix/${first_task.id}`" method="GET" as="div" class="flex flex-row space-x-1 cursor-pointer">
                         <div class="bg-yellow-200 text-violet-500 px-1"> {{ second_task.keyword }} </div>
                         <div :class="second_task.is_completed ? 'line-through' : ''">{{ second_task.description }}</div>
-                    </div>
+                    </Link>
                     <div class="flex flex-row space-x-2 cursor-pointer">
                         <span @click="prepareOpenQuickEditModal(second_task)" class="material-symbols-outlined text-xl opacity-50 hover:opacity-80">edit</span>
                         <span @click="prepareOpenCommentModal(second_task)" class="material-symbols-outlined text-xl opacity-50 hover:opacity-80">add_comment</span>
@@ -40,6 +40,7 @@
 
 <script setup>
 import { useGeneralStore, useLayoutStore, } from '@/Stores/index'
+import { Link } from '@inertiajs/vue3';
 
 const layoutStore = useLayoutStore();
 const generalStore = useGeneralStore();
