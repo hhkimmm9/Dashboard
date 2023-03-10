7b2a669f8ef70df5169720dbb48f4058de1a9985
Harrison Kim <hsookim90@gmail.com>
2023-03-09 19:15:32 -0500

    Convert TaskController to a resource controller

--- routes/web.php
+++ routes/web.php
@@ -37,11 +37,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/modules/blocksix', [TaskController::class, 'index'])->name('blocksix.index');
-    Route::get('/modules/blocksix/create', [TaskController::class, 'create'])->name('blocksix.create');
-    Route::post('/modules/blocksix', [TaskController::class, 'store'])->name('blocksix.store');
-    Route::post('/modules/blocksix/{id}', [TaskController::class, 'show'])->name('blocksix.show');
-    Route::patch('/modules/blocksix/{id}', [TaskController::class, 'update'])->name('blocksix.update');
+    Route::resource('blocksix', TaskController::class);
 
     Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
     Route::get('/settings/history', [SettingsController::class, 'history'])->name('settings.history');
