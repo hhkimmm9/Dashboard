f9b370e05e758a4701f79662e7a7b16e5fcf9124
Harrison Kim <hsookim90@gmail.com>
2023-03-09 20:20:51 -0500

    Create a show page
    - task info
    - subtask tree (looking for a library for this)
    - comment section

--- resources/js/Pages/BlockSix/Show.vue
+++ resources/js/Pages/BlockSix/Show.vue
@@ -7,11 +7,36 @@
                 <div class="relative bg-white border border-gray-200 shadow w-2/3 mx-auto p-6 space-y-4 rounded-lg">
                     <div class="flex flex-row items-center space-x-2 border rounded-full px-2 py-1 w-min">
                         <span class="material-symbols-outlined">keyboard_backspace</span>
-                        <span class="text-sm whitespace-nowrap">Go Back</span>
+                        <Link href="/blocksix" as="span" class="text-sm whitespace-nowrap cursor-pointer"> Go Back </Link>
                     </div>
-                    <div>
-                        <div class="flex">
-                            <input type="checkbox">
+                    <div class="space-y-5">
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-center space-x-2">
+                                <input type="checkbox" :checked="target_task.is_completed">
+                                <span class="bg-yellow-200 px-2">{{ target_task.keyword }}</span>
+                                <span :class="target_task.is_completed ? 'line-through' : ''">{{ target_task.description }}</span>
+                            </div>
+                            <span class="material-symbols-outlined text-lg">edit</span>
+                        </div>
+                        <div class="border border-gray-200 rounded p-4 h-60">
+                            subtask tree
+                        </div>
+                        <div class="border border-gray-200 rounded p-4">
+                            <h3 class="font-bold text-xl mb-3">Comments</h3>
+                            <div class="space-y-4 h-60 overflow-y-auto">
+                                <div v-for="(item, index) in comments" :key="index" class="border p-3 rounded">
+                                    {{ item.content }}
+                                    <hr class="my-2">
+                                    <div class="flex justify-between items-center">
+                                        <div class="text-xs text-gray-500"> {{ item.created_at }} </div>
+                                        <div class="space-x-2">
+                                            <!-- TODO: tooltip -->
+                                            <span class="material-symbols-outlined text-lg cursor-pointer">edit</span>
+                                            <span class="material-symbols-outlined text-lg cursor-pointer">delete</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -23,8 +48,10 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 
+
 const props = defineProps([
     'target_task',
+    'comments',
 ])
 
 </script>
