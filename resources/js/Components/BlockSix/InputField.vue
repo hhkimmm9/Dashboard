9cb61717eee1204448efe451ea4059ef14beb39c
Harrison Kim <hsookim90@gmail.com>
2023-03-20 15:25:51 -0400

    Cleanup - the create page

--- resources/js/Components/BlockSix/InputField.vue
+++ resources/js/Components/BlockSix/InputField.vue
@@ -1,46 +1,60 @@
 <template>
     <div>
         <div :class="[
-            'flex flex-row items-center p-2 w-full',
-            (is_keyword_input_activated | is_description_input_activated) ? 'bg-violet-300 rounded-md' : ''
+            'flex flex-row items-center p-2',
+            (isKeywordOnFocus | isDescriptionOnFocus) ? 'bg-violet-300 rounded-md' : ''
         ]">
-            <!-- input for keyword -->
-            <div v-if="is_keyword_entered == false" :class="'w-full flex space-x-2 justify-center rounded'">
-                <input type="text" v-model="keyword"
-                    @focus="is_keyword_input_activated = true" @blur="is_keyword_input_activated = false"
-                    @keydown.enter="is_keyword_entered = true"
-                    class="
-                        block w-full border-none text-violet-700 font-bold rounded
-                        focus:ring-0
-                        placeholder:opacity-50 placeholder:text-violet-700
-                    "
-                    placeholder="Please enter a keyword for this block 😙"
-                >
-                <button @click="is_keyword_entered = true" ref="next"
-                    :class="['text-violet-700 rounded px-3 py-2 font-bold bg-yellow-200',
-                        keyword.length > 0 ? 'visible' : 'hidden'
-                    ]"
-                > Next </button>
-            </div>
-
-            <!-- input for description -->
-            <div v-else class="w-full">
-                <div class="flex flex-row items-center space-x-2">
-                    <span ref="keyword_container" @click="switchBlockType" class="border p-2 bg-yellow-200 rounded border-none cursor-default text-violet-600 font-bold w-1/4 truncate
-                        hover:w-full transition ease-out duration-200
-                    ">{{ keyword }}</span>
-                    <input type="text"
-                        @focus="is_description_input_activated = true" @blur="clickHandler($event.target.value)"
-                        @keydown.enter="clickHandler($event.target.value)"
+            <div class="flex flex-col w-full">
+                <!-- input for keyword -->
+                <div v-if="isKeywordEntered == false" :class="'w-full flex space-x-2 justify-center rounded'">
+                    <input
+                        v-model="keyword" type="text"
+                        @focus="isKeywordOnFocus = true" @blur="isKeywordOnFocus = false"
+                        @keydown.enter="isKeywordEntered = true"
+                        placeholder="Please enter a keyword 😙"
                         class="
-                            block w-full border-none text-violet-700 font-bold rounded bg-pink-50
+                            block w-full border-none text-violet-700 font-bold rounded
+                            placeholder:opacity-50 placeholder:text-violet-700 placeholder:text-center
                             focus:ring-0
-                            placeholder:opacity-50 placeholder:text-violet-700
+                            sm:placeholder:text-left
                         "
-                        placeholder="Please add a short description 📝"
-                    />
+                    >
+                    <button @click="isKeywordEntered = true" ref="tippyNext"
+                        :class="[
+                            'text-violet-700 rounded px-3 py-2 font-bold bg-yellow-200',
+                            keyword.length > 0 ? 'visible' : 'hidden'
+                        ]"
+                    > Next </button>
                 </div>
 
+                <!-- after an user enters keyword -->
+                <div v-else>
+                    <div class="flex flex-row items-center space-x-2">
+                        <!-- keyword in a yellow background box -->
+                        <span @click="switchBlockType"
+                            class="
+                                border p-2 bg-yellow-200 rounded border-none cursor-default text-violet-600 font-bold w-1/4 truncate transition ease-out duration-200
+                                hover:w-full
+                            "
+                        > {{ keyword }} </span>
+                        <!-- input for description -->
+                        <input
+                            v-model="description" type="text"
+                            @focus="isDescriptionOnFocus = true" @blur="buildInput()"
+                            @keydown.enter="buildInput()"
+                            placeholder="Please add a short description 📝"
+                            class="
+                                block w-full border-none text-violet-700 font-bold rounded bg-pink-50
+                                placeholder:opacity-50 placeholder:text-violet-700
+                                focus:ring-0
+                            "
+                            autofocus
+                        />
+                    </div>
+                </div>
+                <div v-show="showFeedback && !isDescriptionOnFocus" class="mt-1">
+                    <p class="text-red-500 text-sm text-left">Oops, it looks like you forgot to put some description 😅</p>
+                </div>
             </div>
         </div>
         
@@ -49,48 +63,45 @@
 
 <script setup>
 import { ref } from '@vue/reactivity';
-// 이거 쓰는데마다 다 import 하기 싫어
+import { computed, onMounted, onUpdated, watch } from 'vue';
 import { useTippy } from 'vue-tippy';
 
 const emit = defineEmits(['update:modelValue']);
 
-const keyword_container = ref();
+const isKeywordOnFocus = ref(false);
+const isDescriptionOnFocus = ref(false);
 const keyword = ref('');
-const is_keyword_entered = ref(false);
-const is_keyword_input_activated = ref(false);
-const is_description_input_activated = ref(false);
+const isKeywordEntered = ref(false);
+const description = ref('');
+const showFeedback = ref(false);
 
-const clickHandler = (value) => {
-    is_keyword_input_activated.value = false
-    is_description_input_activated.value = false
+function buildInput() {
+    isKeywordOnFocus.value = false
+    isDescriptionOnFocus.value = false
 
     // validation
-    if (keyword.value.length > 0 && value.length > 0) {
-        emit('update:modelValue', keyword.value + '::' + value);
+    if (keyword.value.length > 0 && description.value.length > 0) {
+        emit('update:modelValue', keyword.value + '::' + description.value);
+        showFeedback.value = false;
     }
-    else if (value.length == 0) {
-        // feedback
+    else if (description.value.length == 0) {
+        showFeedback.value = true;
     }
 };
 
-const switchBlockType = () => {
+function switchBlockType() {
     // core - red
     // general - yellow
     // rest - green
 };
 
 // Tippy
-const next = ref();
-useTippy(next, {
+const tippyNext = ref();
+useTippy(tippyNext, {
     content: "Click to add a short description 😗",
     placement: 'top',
     animation: 'shift-away',
     theme: 'light',
     duration: [300, 0],
 })
-
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- resources/js/Pages/BlockSix/Create.vue
+++ resources/js/Pages/BlockSix/Create.vue
@@ -2,7 +2,10 @@
     <Head title="Block Six" />
 
     <AuthenticatedLayout class="relative">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-32 h-full bg-violet-100 py-10">
+        <div class="
+            min-w-fit max-w-7xl mx-auto h-full bg-violet-100 py-10
+            sm:px-6 lg:px-32
+        ">
             <div class="flex justify-center">
                 <div class="flex flex-col cursor-default text-center">
                     <!-- Import Google Font and utilize it within Tailwind -->
@@ -15,10 +18,14 @@
             <div class="mt-8">
                 <form @submit.prevent="createTasks">
                     <div class="flex justify-center text-center">
-                        <div class="bg-white border shadow w-2/3 p-6 flex flex-col space-y-4 rounded-lg">
+                        <div class="
+                            bg-white border shadow rounded-lg flex flex-col
+                            p-3 space-y-3 w-full mx-2
+                            sm:w-2/3 sm:p-6 sm:mx-0
+                        ">
                             <div class="border-2 p-3 rounded-lg shadow">
                                 <div class="text-xl font-bold cursor-default my-2 text-violet-800">Morning 🕘</div>
-                                <InputField @update:modelValue="(keyword_and_description) => buildInput('slot1', keyword_and_description)" />
+                                <InputField v-model="tasks.slot1" />
                                 <InputField v-model="tasks.slot2" />
                             </div>
                             <div class="border-2 p-3 rounded-lg shadow">
@@ -34,20 +41,25 @@
                         </div>
                     </div>
                     <div class="flex justify-center mt-10">
-                        <button type="submit"
-                            class="bg-yellow-200 hover:bg-yellow-300 px-4 py-2 rounded-lg text-violet-700 font-bold shadow-md text-xl"
-                        > Set </button>
+                        <button type="submit" :disabled="isDisabled"
+                            :class="[
+                                'px-4 py-2 rounded-lg text-violet-700 font-bold shadow-md text-xl',
+                                isDisabled ? 'bg-gray-200' : 'hover:text-2xl hover:bg-yellow-300 bg-yellow-200'
+                            ]"
+                        > Submit </button>
                     </div>
                 </form>
             </div>
         </div>
 
         <!-- TODO: transition -->
-        <div ref="purchase_book" class="
-            absolute right-4 bottom-4 w-52 cursor-pointer invisible
-            hover:w-60
-            lg:visible
-        ">
+        <div ref="tippyPurchaseBook"
+            class="
+                fixed right-4 bottom-4 w-52 cursor-pointer invisible
+                hover:w-60
+                lg:visible
+            "
+        >
             <a href="http://www.yes24.com/Product/Goods/104868817" target="_blank">
                 <img src="/storage/img/blocksix_bookcover.jpeg" alt="">
             </a>
@@ -58,8 +70,10 @@
 <script setup>
 import InputField from '@/Components/BlockSix/InputField.vue'
 import { useForm } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { useTippy }from 'vue-tippy'
+
+const isDisabled = ref(true);
 
 const tasks = useForm({
     slot1: null,
@@ -68,29 +82,33 @@ const tasks = useForm({
     slot4: null,
     slot5: null,
     slot6: null,
+    type: 'task'
 });
-
-const buildInput = (whichSlot, keyword_and_description) => {
-    // TODO
-    if (whichSlot == 'slot1') tasks.slot1 = keyword_and_description;
-    else if (whichSlot == 'slot2') tasks.slot2 = keyword_and_description;
-    else if (whichSlot == 'slot3') tasks.slot3 = keyword_and_description;
-    else if (whichSlot == 'slot4') tasks.slot4 = keyword_and_description;
-    else if (whichSlot == 'slot5') tasks.slot5 = keyword_and_description;
-    else if (whichSlot == 'slot6') tasks.slot6 = keyword_and_description;
-}
 
 function createTasks() {
     tasks.post('/blocksix', {
-        onSuccess: () => {
-            tasks.reset();
-        },
+        onSuccess: () => tasks.reset(),
     });
 };
 
+watchEffect(() => {
+    if (
+        tasks.slot1 &&
+        tasks.slot2 &&
+        tasks.slot3 &&
+        tasks.slot4 &&
+        tasks.slot5 &&
+        tasks.slot6
+    ) {
+        isDisabled.value = false;
+    }
+    
+    else isDisabled.value = true;
+});
+
 // Tippy
-const purchase_book = ref();
-useTippy(purchase_book, {
+const tippyPurchaseBook = ref();
+useTippy(tippyPurchaseBook, {
     content: "Click to buy the book!",
     placement: 'top',
     animation: 'shift-away',
