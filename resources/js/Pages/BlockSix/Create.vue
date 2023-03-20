47665a2c82b489ea7edccac3992857da33f88c97
Harrison Kim <hsookim90@gmail.com>
2023-03-20 12:44:35 -0400

    Change the styling of the title on the create page

--- resources/js/Pages/BlockSix/Create.vue
+++ resources/js/Pages/BlockSix/Create.vue
@@ -4,9 +4,11 @@
     <AuthenticatedLayout class="relative">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-32 h-full bg-violet-100 py-10">
             <div class="flex justify-center">
-                <div class="flex flex-col font-bold text-3xl text-purple-800 cursor-default text-center font-noto-sans-korean-900">
-                    <span>시간을 선택하는 기술</span>
-                    <span>블럭식스</span>
+                <div class="flex flex-col cursor-default text-center">
+                    <!-- Import Google Font and utilize it within Tailwind -->
+                    <p class="font-noto-sans-korean font-bold text-3xl text-purple-800">
+                        시간을 선택하는 기술<br>블럭식스
+                    </p>
                 </div>
             </div>
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,9 +8,11 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700;800&display=swap">
+
+        <!-- Noto Sans Korean -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@700&display=swap" rel="stylesheet">
 
         <!-- Google Material Icons -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
