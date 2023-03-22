0198608ea7500a5b1fa936715bf51dc3770419d2
Harrison Kim <hsookim90@gmail.com>
2023-03-21 21:50:08 -0400

    quickfix - import comment-controller

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use App\Models\Task;
 use Inertia\Inertia;
 use Illuminate\Support\Carbon;
-use Illuminate\Console\Application;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\SettingsController;
