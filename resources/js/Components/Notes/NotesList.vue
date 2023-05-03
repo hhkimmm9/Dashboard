f8b27d103a6676b9852894755a98a58ea21fe72f
Harrison Kim <hsookim90@gmail.com>
2023-05-03 10:47:18 -0400

    Fix the logic for notesList

--- resources/js/Components/Notes/NotesList.vue
+++ resources/js/Components/Notes/NotesList.vue
@@ -4,14 +4,18 @@
             <p class="font-bold text-2xl"> Notes </p>
         </div>
         <div class="h-full overflow-y-auto">
-            <ul v-if="notesFilteredByFolder.length > 0" class="mt-2 space-y-2">
+            <ul v-if="!showCreateButton" class="mt-2 space-y-2">
                 
                     <li v-for="(item, index) in notesFilteredByFolder" :key="index">
                         <NoteItem :note="item" />
                     </li>
             </ul>
-            <div v-else class="text-center mt-2">
-                <p>Empty</p>
+            <div v-else class="flex justify-center mt-5">
+                <Link href="notes/create" as="button"
+                    class="py-2 rounded w-full mx-5 bg-gray-50 shadow hover:bg-gray-100"
+                >
+                    <p class="font-medium">Create a new note</p>
+                </Link>
             </div>
         </div>
     </div>
@@ -20,19 +24,32 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
 import { useGeneralStore } from '@/Stores/index'
-import { computed } from 'vue';
+import { ref, watchEffect } from 'vue';
 
 import NoteItem from '@/Components/Notes/NoteItem.vue'
 
 const generalStore = useGeneralStore()
 
-const notesFilteredByFolder = computed(() => {
+const showCreateButton = ref(true)
+var notesFilteredByFolder = ref([])
+
+watchEffect(() => {
+    // all folders
     if (generalStore.selectedFolderId == 0) {
-        return generalStore.notes
+        notesFilteredByFolder.value = generalStore.notes.filter(item => item.deleted_at == null)
     }
+    // deleted folders
+    else if (generalStore.selectedFolderId == -1) {
+        notesFilteredByFolder.value = generalStore.notes.filter(item => item.deleted_at != null)
+    }
+    // general folders
     else {
-        return generalStore.notes.filter(item => item.folder_id == generalStore.selectedFolderId )
+        notesFilteredByFolder.value = generalStore.notes.filter(item => item.folder_id == generalStore.selectedFolderId )
     }
+})
+
+watchEffect(() => {
+    showCreateButton.value = notesFilteredByFolder.value == 0 ? true : false
 })
 </script>
 
