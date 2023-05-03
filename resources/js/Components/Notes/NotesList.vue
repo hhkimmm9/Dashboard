f250e5789040e8e8afe9db0d04c9032eb0033161
Harrison Kim <hsookim90@gmail.com>
2023-05-03 00:32:00 -0400

    Create NoteItem component

--- resources/js/Components/Notes/NotesList.vue
+++ resources/js/Components/Notes/NotesList.vue
@@ -4,44 +4,13 @@
             <p class="font-bold text-2xl"> Notes </p>
         </div>
         <div class="h-full overflow-y-auto">
-            <ul v-if="notesFilteredByFolder.lenght > 0" class="mt-2 space-y-2">
-                <li v-for="(item, index) in notesFilteredByFolder" :key="index" class="flex justify-between items-center hover:bg-gray-100 px-2">
-                    <Link :href="route('notes.show', { id: item.id })" class="cursor-pointer truncate mr-5"> {{ item.label }} </Link>
-                    <Dropdown>
-                        <template v-slot:trigger>
-                            <span class="material-symbols-outlined text-lg cursor-pointer"> more_horiz </span>
-                        </template>
-                        <template v-slot:content>
-                            <div class="flex flex-col px-3 py-1">
-                                <ul>
-                                    <Link
-                                        :href="route('notes.edit', { id: item.id })"
-                                        as="li"
-                                        class="
-                                            flex items-center gap-2 cursor-pointer
-                                            hover:bg-gray-50
-                                        "
-                                    >
-                                        <span class="material-symbols-outlined text-lg"> edit </span>
-                                        <span> Edit </span>
-                                    </Link>
-                                    <Link
-                                        :href="route('notes.destroy', { id: item.id })"
-                                        method="DELETE"
-                                        as="li"
-                                        class="
-                                            flex items-center gap-2 cursor-pointer
-                                            hover:bg-gray-50
-                                        "
-                                    >
-                                        <span class="material-symbols-outlined text-lg"> delete </span>
-                                        <span> Delete </span>
-                                    </Link>
-                                </ul>
-                            </div>
-                        </template>
-                    </Dropdown>
-                </li>
+            <ul v-if="notesFilteredByFolder.length > 0" class="mt-2 space-y-2">
+                
+                    <li v-for="(item, index) in notesFilteredByFolder" :key="index">
+                        <Link :href="`/notes/${item.id}`">
+                            <NoteItem :note="item" />
+                        </Link>
+                    </li>
             </ul>
             <div v-else class="text-center mt-2">
                 <p>Empty</p>
@@ -51,10 +20,11 @@
 </template>
 
 <script setup>
-import Dropdown from '@/Components/Dropdown.vue'
+import { Link } from '@inertiajs/vue3';
 import { useGeneralStore } from '@/Stores/index'
 import { computed } from 'vue';
-import { Link } from '@inertiajs/vue3';
+
+import NoteItem from '@/Components/Notes/NoteItem.vue'
 
 const generalStore = useGeneralStore()
 
